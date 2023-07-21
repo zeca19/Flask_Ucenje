159ed40e19a8ca417849a1ad76086dbe0edfecfd
Luka Zecevic <lz20201092@student.fon.bg.ac.rs>
2023-07-21 18:29:43 +0200

    Dodata about stranica i naucene promene uzimanja podataka

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 # Ovo predstavlja rutu u koju ulazimo
 
+# Dodajemo neke podatke
+
+postovi = [
+    {
+        "autor": "Nenad Bojanic",
+        "naslov": "Blog Post 1",
+        "content": "Prvi post content",
+        "datum_objave": "Jul 21, 2023"
+    },
+    {
+        "autor": "Goran Predragovic",
+        "naslov": "Blog Post 2",
+        "content": "Drugi post content",
+        "datum_objave": "Jul 21, 2023"
+    }
+]
+
 
 @app.route("/")
 def hello():
-    return "<h2>Zdravo</h2>"
+    return render_template("layout.html", postovi=postovi, title="Pocetna Stranica")
+
+
+@app.route("/about")
+def about():
+    return render_template("layout.html", postovi=postovi, title="About stranica")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
