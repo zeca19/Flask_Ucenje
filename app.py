b7760d0885d54a1522baf996bd0aa78b2d14becd
Luka Zecevic <lz20201092@student.fon.bg.ac.rs>
2023-07-23 16:47:05 +0200

    Dodati novi template za login, promenjena biznis logika i forma za login i register i update-ovan requirements.txt

--- app.py
+++ app.py
@@ -1,37 +1,56 @@
-from flask import Flask, render_template,url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-# Ovo predstavlja rutu u koju ulazimo
-
-# Dodajemo neke podatke
-
-postovi = [
+posts = [
     {
-        "autor": "Nenad Bojanic",
-        "naslov": "Blog Post 1",
-        "content": "Prvi post content",
-        "datum_objave": "Jul 21, 2023"
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
     {
-        "autor": "Goran Predragovic",
-        "naslov": "Blog Post 2",
-        "content": "Drugi post content",
-        "datum_objave": "Jul 21, 2023"
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
 
 
 @app.route("/")
-def hello():
-    return render_template("home.html", postovi=postovi, title="Pocetna Stranica")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", postovi=postovi, title="About stranica")
+    return render_template('about.html', title='About')
 
 
-if __name__ == "__main__":
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,17 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired,Length,Email,EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
 
 class RegistrationForm(FlaskForm):
-     username=StringField(label="Username",validators=[DataRequired(),Length(min=2,max=20)])
-     email=StringField(label="Email",validators=[DataRequired(),Email()])
-     password=PasswordField("Password",validators=[DataRequired()])
-     confirm_password=PasswordField("Confirm Password",validators=[DataRequired(),EqualTo("password")])
-     submit=SubmitField("Sign Up")
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
-     email=StringField(label="Email",validators=[DataRequired(),Email()])
-     password=PasswordField("Password",validators=[DataRequired()])
-     remember=BooleanField("Remember me")
-     submit=SubmitField("Login")
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
