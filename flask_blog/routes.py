d35706073c6487e5ea8966ade1c77d1d3b73b8b3
Luka Zecevic <lz20201092@student.fon.bg.ac.rs>
2023-07-28 18:36:42 +0200

    Napravljen novi korisnik i ispravljena slovna greska

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -34,7 +34,7 @@ def register():
         user=User(username=form.username.data,email=form.email.data,password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash(f'Your accuount has been created! You are now able to log in', 'success')
+        flash(f'Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
